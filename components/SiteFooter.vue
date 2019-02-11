d75647b10669602526faca70d32a3f0d5da31ae5
mhe <mail@marcus-herrmann.com>
2019-02-11 11:24:15 +0100

    Fixes tracking id for matomo

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -21,7 +21,7 @@
           (function() {
             var u="//stat.marcus-herrmann.com/";
             _paq.push(['setTrackerUrl', u+'piwik.php']);
-            _paq.push(['setSiteId', '7']);
+            _paq.push(['setSiteId', '8']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
             g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
           })();
