5d1f836fecb2a5e334209d2a2964a83965746100
Regis Philibert <login@regisphilibert.com>
2019-02-11 20:16:14 -0500

    Add a Hugo resource
    I thought Hugo could use a more "Set up a new blog using _____" piece featured in there. This is a great one by https://github.com/zwbetz-gh

--- components/HowTos.vue
+++ components/HowTos.vue
@@ -7,6 +7,7 @@
         <li><a href="https://www.filamentgroup.com/lab/build-a-blog/">Set up a new blog using <b>Eleventy</b></a></li>
         <li><a href="https://codeburst.io/build-a-blog-using-gatsby-js-react-8561bfe8fc91">Set up a new blog using <b>Gatsby</b></a></li>
         <li><a href="https://medium.com/@adam.collier/creating-a-blog-with-vuepress-44ec0fed9718">Set up a new blog using <b>VuePress</b></a></li>
+        <li><a href="https://zwbetz.com/make-a-hugo-blog-from-scratch/">Set up a new blog using <b>Hugo</b></a></li>
         <li><a href="https://www.sarasoueidan.com/blog/jekyll-ghpages-to-hugo-netlify/">Migrating from Jekyll/GitHub Pages to <b>Hugo</b> and Netlify</a></li>
         <li><a href="https://regisphilibert.com/blog/2019/01/from-wordpress-to-hugo-a-mindset-transition/">From WordPress to <b>Hugo</b></a></li>
         <li><a href="http://www.solomon-blog.com/posts/kirby-blog">Set up a new blog using <b>Kirby CMS</b></a></li>
